90db6cf96b719362d6b10b8b05823a54de252bdd
Melissa He <melissahe@ac.c4q.nyc>
2017-11-28 18:07:54 -0500

    Initial Commit

---
+++ LoadingImages-Exercises/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoadingImages-Exercises
+//
+//  Created by C4Q on 11/28/17.
+//  Copyright © 2017 Melissa He @ C4Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
